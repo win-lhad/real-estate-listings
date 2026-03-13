5fb98f5ad0faeb3bf2d9f317976ba05027751e7c
Duy Le <duyhale21@gmail.com>
2026-03-13 15:15:51 +0700

    Create the REL project with AppIcon, AppColors and update .gitignore

---
+++ RealEstateListings/RealEstateListingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealEstateListingsApp.swift
+//  RealEstateListings
+//
+//  Created by Duy Le on 13/3/26.
+//
+
+import SwiftUI
+
+@main
+struct RealEstateListingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
